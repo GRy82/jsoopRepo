ba11dd96f3349f9bf062631ba1d3dc7be1cdc014
Gregory Ryan <gwryan@buffalo.edu>
2021-06-18 11:47:59 -0400

    new object constructor, HtmlImageElement created. This object constructor as well as that of HtmlSelectElement each have their own render functions now. Hypothetically, you could pass either element to a function, and within that function have the parameter's render function no matter what kind of object.

--- ProtoIExercise.js
+++ ProtoIExercise.js
@@ -19,10 +19,26 @@ function HtmlSelectElement(items = []){
         if(!this.items.includes(item)) throw new Error('that item is not in items.');
         this.items.splice(this.items.indexOf(item), 1);
     };
+
+    this.render = function(){
+        return `<select>${this.items.map(el => 
+            `<option>${el}</option>`)
+            .join('')}</select>`;
+    }
+}
+
+function HtmlImageElement(src = undefined){
+    this.src = src;
+
+    this.render = function(){
+        return `<img src="${this.src}" />`;
+    };
 }
 
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
 
 
 
