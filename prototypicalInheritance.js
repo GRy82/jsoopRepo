da4f7d572e8d59643a73bd8c2bf897616c2b50ae
Gregory Ryan <gwryan@buffalo.edu>
2021-06-17 22:52:59 -0400

    Circle regains its original constructor with an explicit assignment, following its prototypical inheritance of Shape.

--- prototypicalInheritance.js
+++ prototypicalInheritance.js
@@ -16,5 +16,8 @@ Shape.prototype.draw = function(){
 }
 
 Circle.prototype = Object.create(Shape.prototype);
+//this line gives the Circle access to its constructor again.
+//without this, the only constructor it has access to is its base's, ie. Shape's constructor.
+Circle.prototype.constructor = Circle;
 
 const circle = new Circle(5);
