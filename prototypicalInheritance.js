57cb64abbbebb985e29597e3e088d7781b75aac2
Gregory Ryan <gwryan@buffalo.edu>
2021-06-17 23:02:44 -0400

    arguments now are passed to Shape constructor. For Circle objects to be able to use the Shape constructor, it must use the call method.

--- prototypicalInheritance.js
+++ prototypicalInheritance.js
@@ -1,23 +1,27 @@
 
-function Shape(){
-
+function Shape(color, texture){
+    this.color = color;
+    this.texture = texture;
 }
 
-function Circle(radius){
+function Circle(radius, color, texture){
+    Shape.call(this, color, texture);
+    
     this.radius = radius;
 }
 
+Shape.prototype.draw = function(){
+    
+}
+
+//this line declares the prototypical inheritance. 
+Circle.prototype = Object.create(Shape.prototype);
+//thi code must be after the inheritance if it is to maintain the spin function.
 Circle.prototype.spin = function(){
     console.log('The circle is spinning.');
 }
-
-Shape.prototype.draw = function(){
-
-}
-
-Circle.prototype = Object.create(Shape.prototype);
 //this line gives the Circle access to its constructor again.
 //without this, the only constructor it has access to is its base's, ie. Shape's constructor.
 Circle.prototype.constructor = Circle;
 
-const circle = new Circle(5);
+const circle = new Circle(5, 'red', 'smooth');
